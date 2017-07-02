946bc75ab6341ec2c6597a9e5df369ab373e3376
JerryHu <jerryhu@JerryHus-Mac-mini.local>
2017-07-02 13:10:28 -0400

    First Commit go primitive data type

---
+++ GettingStarted/src/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("ggg")
+	println("Hello Go")
+
+	var myInt int
+	myInt = 42
+
+	println(myInt)
+
+	var myFloat32 float32 = 42.
+	println(myFloat32)
+
+	myString := "Hello Go"
+	println(myString)
+
+	myComplex := complex(2, 3)
+	println(myComplex)
+	println(real(myComplex))
+	println(imag(myComplex))
+
+}
