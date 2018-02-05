ac94c3987266051451db070c5e73c8f235ff5b45
JerryHu <jerryhu@JerryHus-Mac-mini.local>
2018-02-05 14:47:40 -0500

    07 Methods in Go Programming

---
+++ src/GolangProgramming/struct.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+const usixteenbitmax float64 = 65535
+const kmh_multiple float64 = 1.60934
+
+type car struct {
+	gas_pedal uint16     //min: 0,      max: 65535    16bit
+	brake_pedal uint16   //min: 0,      max: 65535
+	steering_wheel int16 //min: -32768  max: 32768
+	top_speed_kmh float64 //what's our top speed?
+}
+
+func (c car) kmh() float64 {
+	return float64(c.gas_pedal) * (c.top_speed_kmh/usixteenbitmax)
+}
+
+func (c car) mph() float64 {
+	return float64(c.gas_pedal) * (c.top_speed_kmh/kmh_multiple/usixteenbitmax)
+}
+
+func main() {
+	//a_car := car{gas_pedal: 16535, brake_pedal: 0, steering_wheel: 12562, top_speed_kmh: 225.0}
+	a_car := car{22314,0,12562,225.0}
+	fmt.Println("gas_pedal:",a_car.gas_pedal, "brake_pedal:",a_car.brake_pedal,"steering_wheel:",a_car.steering_wheel)
+	fmt.Println(a_car.kmh())
+	fmt.Println(a_car.mph())
+}
