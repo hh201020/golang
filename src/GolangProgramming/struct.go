a78cb62d22c85b28f7a13e50412d9a97b74ca552
JerryHu <jerryhu@JerryHus-Mac-mini.local>
2018-02-06 20:59:41 -0500

    08 Pointer Receivers in Go Programming

--- src/GolangProgramming/struct.go
+++ src/GolangProgramming/struct.go
@@ -12,18 +12,26 @@ type car struct {
 	top_speed_kmh float64 //what's our top speed?
 }
 
+//gets a copy, receiver type
 func (c car) kmh() float64 {
-	return float64(c.gas_pedal) * (c.top_speed_kmh/usixteenbitmax)
+	return float64(c.gas_pedal) * (c.top_speed_kmh/usixteenbitmax) //top speed: 225. So do pedal * (225/65535)
 }
 
+//gets a copy, receiver type
 func (c car) mph() float64 {
-	return float64(c.gas_pedal) * (c.top_speed_kmh/kmh_multiple/usixteenbitmax)
+	return float64(c.gas_pedal) * (c.top_speed_kmh/kmh_multiple/usixteenbitmax)  //top speed: 140 mph.So do pedal * (225/1.60934/65535)
+}
+
+//modify the struct itself via pointer type
+func (c *car) new_top_speed(newspeed float64) {
+	c.top_speed_kmh = newspeed
 }
 
 func main() {
-	//a_car := car{gas_pedal: 16535, brake_pedal: 0, steering_wheel: 12562, top_speed_kmh: 225.0}
+	//a_car := car{gas_pedal: 16535, brake_pedal: 0, steering_wheel: 12562}
 	a_car := car{22314,0,12562,225.0}
 	fmt.Println("gas_pedal:",a_car.gas_pedal, "brake_pedal:",a_car.brake_pedal,"steering_wheel:",a_car.steering_wheel)
-	fmt.Println(a_car.kmh())
-	fmt.Println(a_car.mph())
+	fmt.Println("Car is going",a_car.mph(),"MPH,",a_car.kmh(),"KMH, and top speed is",a_car.top_speed_kmh)
+	a_car.new_top_speed(500)
+	fmt.Println("Car is going",a_car.mph(),"MPH,",a_car.kmh(),"KMH, and top speed is",a_car.top_speed_kmh)
 }
