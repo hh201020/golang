089e62202f015401ed2b889b848a17e845634095
JerryHu <jerryhu@JerryHus-Mac-mini.local>
2018-02-08 14:04:19 -0500

    11 Looping in Go Programming

--- src/GolangProgramming/accessInternet.go
+++ src/GolangProgramming/accessInternet.go
@@ -3,37 +3,31 @@ package main
 import (
 	"encoding/xml"
 	"fmt"
-  )
-  
-  var washPostXML = []byte(`
-  <sitemapindex>
-	 <sitemap>
-		<loc>http://www.washingtonpost.com/news-politics-sitemap.xml</loc>
-	 </sitemap>
-	 <sitemap>
-		<loc>http://www.washingtonpost.com/news-blogs-politics-sitemap.xml</loc>
-	 </sitemap>
-	 <sitemap>
-		<loc>http://www.washingtonpost.com/news-opinions-sitemap.xml</loc>
-	 </sitemap>
-  </sitemapindex>
-  `)
-  
-  type Sitemapindex struct {
+	"io/ioutil"
+	"net/http"
+)
+
+type Sitemapindex struct {
 	Locations []Location `xml:"sitemap"`
-  }
-  
-  type Location struct {
+}
+
+type Location struct {
 	Loc string `xml:"loc"`
-  }
-  
-  func (e Location) String () string {
-	return fmt.Sprintf(e.Loc)
-  }
-  
-  func main() {
-	bytes := washPostXML
+}
+
+func (l Location) String() string {
+	return fmt.Sprintf(l.Loc)
+}
+
+func main() {
+	resp, _ := http.Get("https://www.washingtonpost.com/news-sitemap-index.xml")
+	bytes, _ := ioutil.ReadAll(resp.Body)
+
 	var s Sitemapindex
 	xml.Unmarshal(bytes, &s)
-	fmt.Println(s.Locations)
-  }
+	//fmt.Println(s.Locations)
+
+	for index, Location := range s.Locations {
+		fmt.Printf("%d %s\n", index, Location)
+	}
+}
